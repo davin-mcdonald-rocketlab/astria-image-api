4dac1f7e40c5a9768cd440a1a0dbce29be1875df
Davin McDonald <davin.mcdonald@rocketlab.com.au>
2024-04-17 10:21:59 +1000

    refactor, add random prompt select helper

--- index.js
+++ index.js
@@ -1,54 +1,130 @@
 import { downloadImage } from './downloadImage.js';
+const {
+  randomInt,
+} = await import('node:crypto');
 import dotenv from 'dotenv';
 dotenv.config();
 
-const TUNE_IDS = {
-  CyberRealistic: '657379',
-  RealisticVision: '690204',
-}
-
-
-const API_URL = `https://api.astria.ai/tunes/${TUNE_IDS.CyberRealistic}/prompts`;
 const API_KEY = process.env.ASTRIA_API_KEY;
 const headers = { Authorization: `Bearer ${API_KEY}` }
 
-const GENDER = {
-  male: 'male, man,',
-  female: 'female, woman',
+const TUNE_IDS = {
+  CyberRealistic_v2: '657379',
+  RealisticVision_v2: '690204',
+  CyberRealistic_v4: '838714',
+  RealisticVision_v5: '678865',
 }
+
+const CONTROLNET_OPT = {
+  lineart: 'lineart',
+  hed: 'hed',
+  pose: 'pose',
+  composition: 'composition',
+  reference: 'reference',
+  ipadapter: 'ipadapter',
+  depth: 'depth',
+  tile: 'tile',
+  canny: 'canny',
+  mlsd: 'mlsd',
+  qr: 'qr',
+}
+
+const generateRandomApiUrl = () => {
+  const tuneIdsKeys = Object.keys(TUNE_IDS);
+  const randomTuneIdKey = tuneIdsKeys[randomInt(tuneIdsKeys.length)];
+  const tuneId = TUNE_IDS[randomTuneIdKey];
   
+  return `https://api.astria.ai/tunes/${tuneId}/prompts`;
+}
 
-// const promptText = `realistic digital painting,  astronaut in a garden on a spring day, by martine johanna and simon stalenhag and chie yoshii and casey weldon and wlop, ornate, dynamic, particulate, rich colors, intricate, elegant, highly detailed, harpers bazaar art, fashion magazine, smooth, sharp focus, 8 k, octane rende --mask_prompt face, head --mask_negative clothes, hair --mask_invert --mask_dilate -20 --hires_denoising_strength 0.1`
-// const promptText = `realistic digital painting, ${gender} space marine, warhammer 40,000, war hammer 40k, storm bolter, terminator armour, apothecary in terminator armour, battle in the background, ornate, dynamic, particulate, rich colors, intricate, highly detailed, harpers bazaar art, smooth, sharp focus, 8 k, octane rende --mask_prompt face, hair, head --mask_negative clothes --mask_invert --mask_dilate -20 --hires_denoising_strength 0.1`
-const promptText = `realistic digital painting, ${GENDER.male} italian, pink hair, ornate, dynamic, particulate, rich colors, intricate, highly detailed, harpers bazaar art, smooth, sharp focus, 8 k, octane rende --mask_prompt face, hair, head --mask_negative clothes --mask_invert --mask_dilate -20 --hires_denoising_strength 0.1`
-// const promptText = `realistic digital painting, ${gender} fireman, firefighter, ornate, dynamic, particulate, rich colors, intricate, highly detailed, realistic, hi-res, harpers bazaar art, smooth, sharp focus, 8 k, octane rende --mask_prompt face, hair, head --mask_negative clothes --mask_invert --mask_dilate -20 --hires_denoising_strength 0.1`
+const GENDER = {
+  male: '((male, man)),',
+  female: '((female, woman)),',
+}
 
-const form = new FormData();
-form.append('prompt[text]', promptText);
-form.append('prompt[num_images]', '1');
-form.append('prompt[negative_prompt]', 'clay, text, watermark, padding, cropped, typography');
-form.append('prompt[seed]', '');
-form.append('prompt[steps]', '50');
-form.append('prompt[cfg_scale]', '');
-form.append('prompt[controlnet]', 'pose');
-form.append('prompt[input_image_url]', 'https://therocketlab.github.io/rktlb-signatures/profile-images/davin-lg.jpg');
-// form.append('prompt[input_image_url]', 'https://therocketlab.github.io/rktlb-signatures/profile-images/wren.jpeg');
-form.append('prompt[mask_image_url]', '');
-form.append('prompt[denoising_strength]', '');
-form.append('prompt[controlnet_conditioning_scale]', '');
-form.append('prompt[controlnet_txt2img]', 'false');
-form.append('prompt[super_resolution]', 'true');
-form.append('prompt[inpaint_faces]', 'false');
-form.append('prompt[face_correct]', 'true');
-form.append('prompt[film_grain]', 'false');
-form.append('prompt[face_swap]', 'false');
-form.append('prompt[hires_fix]', 'true');
-// form.append('prompt[ar]', 1.1);
-form.append('prompt[scheduler]', 'dpm++sde_karras');
-form.append('prompt[color_grading]', '');
-form.append('prompt[use_lpw]', 'true');
-form.append('prompt[w]', '512');
-form.append('prompt[h]', '768');
+// TODO: add camera lens type to the prompt
+const PROMPT = {
+  astronaut: `astronaut in a garden on a spring day, by martine johanna and simon stalenhag and chie yoshii and casey weldon and wlop, ornate`,
+  beerGarden: `Sitting in a beer garden with some drinks`,
+  italian: `italian, pink hair, ornate`,
+  fireman: `fireman, firefighter, ornate`,
+  formal: `Walking to a formal function in a tuxedo / ball gown, ornate`,
+  hike: `On a hike with some trees, nature`,
+  puppies: `Lying with a bunch of puppies, cute, fluffy`,
+  surfing: `On a surfboard with a shark in the background, potential danger`,
+  karaoke: `Singing karaoke with friends, fun, party, bar, pub`,
+  dog: `Snuggling with golden retriever, couch, cute, cozy`,
+  machuPichu: `Standing on Machu Pichu with arms in the air, mountains, nature, travel`,
+  martini: `Smirking with a martini in hand, ornate, party, fun`,
+  iceSkating: `Fallen over ice skating and a smile on face, winter, fun, cold`,
+  baby: `Holding a crying baby looking nervous but happy, cute, baby, family`,
+  gardening: `In a vegetable patch, tending to the plants, nature, gardening`,
+  riding: `Riding a bike on a cobblestone path, nature, fun, exercise`,
+  boardGame: `Laughing with friends with a board game at a pub, fun, party, friends`,
+  pottery: `Concentrating on working a pottery wheel, art, craft, pottery`,
+  swim: `In a lap pool, holding onto the edge, looking up to the camera with sun in their face, exercise, swim, pool`,
+  marathon: `Posing with a medal at the end of a marathon looking sweaty but happy and relieved, exercise, marathon,`,
+  art: `In a museum looking closly at a painting, art, museum, culture`,
+  flower: `Picking a daffodil out of a garden, nature, flower, gardening`,
+  ute: `Sitting in the back of a ute whilst driving in country side with friends around, fun, car, friends, travel`,
+}
+
+const generatePrompt = (prompt, gender) => {
+  return `realistic digital painting, ${gender} ${prompt}, dynamic, particulate, rich colors, intricate, highly detailed, realistic, hi-res, harpers bazaar art, smooth, sharp focus, 8 k, octane rende --mask_prompt face, hair, head --mask_negative clothes --mask_invert --mask_dilate -20 --hires_denoising_strength 0.1`
+}
+
+const generateThreeUniquePrompts = (gender) => {
+  const promptKeys = Object.keys(PROMPT);
+  const selectedPrompts = new Set();
+
+  // Ensure unique selection of prompts
+  while (selectedPrompts.size < 6) {
+    const randomPromptKey = promptKeys[randomInt(promptKeys.length)];
+
+    if (!selectedPrompts.has(randomPromptKey)) {
+      selectedPrompts.add(randomPromptKey);
+    }
+  }
+
+  return Array.from(selectedPrompts).map(promptKey => {
+    const prompt = PROMPT[promptKey];
+    return generatePrompt(prompt, GENDER[gender]);
+  });
+}
+
+// randomly select 6 unique prompts
+const promptArray = generateThreeUniquePrompts('male')
+
+console.log('promptArray: ', promptArray)
+
+const generateFormData = (promptArray, imageURL) => {
+  const form = new FormData();
+  form.append('prompt[text]', promptArray);
+  form.append('prompt[num_images]', '1');
+  form.append('prompt[negative_prompt]', 'clay, text, watermark, padding, cropped, typography');
+  form.append('prompt[seed]', '');
+  form.append('prompt[steps]', '30');
+  form.append('prompt[cfg_scale]', '');
+  form.append('prompt[controlnet]', CONTROLNET_OPT.pose);
+  form.append('prompt[input_image_url]', imageURL);
+  form.append('prompt[mask_image_url]', '');
+  form.append('prompt[denoising_strength]', '');
+  form.append('prompt[controlnet_conditioning_scale]', '');
+  form.append('prompt[controlnet_txt2img]', 'false');
+  form.append('prompt[super_resolution]', 'true');
+  form.append('prompt[inpaint_faces]', 'false');
+  form.append('prompt[face_correct]', 'true');
+  form.append('prompt[film_grain]', 'false');
+  form.append('prompt[face_swap]', 'false');
+  form.append('prompt[hires_fix]', 'true');
+  // form.append('prompt[ar]', 1.1);
+  form.append('prompt[scheduler]', 'dpm++sde_karras');
+  form.append('prompt[color_grading]', '');
+  form.append('prompt[use_lpw]', 'true');
+  form.append('prompt[w]', '512');
+  form.append('prompt[h]', '768');
+  return form;
+}
 
 const fetchJson = async (url) => {
   const response = await fetch(url, { headers: headers });
@@ -71,10 +147,18 @@ const fetchJson = async (url) => {
   }
 };
 
+const images = {
+  davin: 'https://therocketlab.github.io/rktlb-signatures/profile-images/davin-lg.jpg',
+  random: 'https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?t=st=1713312958~exp=1713316558~hmac=83aed9a33b9a59324cfc4fd592a2f3d78f04cacc08a1d8e46b845f24a4654917&w=2000'
+}
+
 const getImage = async () => {
+  // TODO: loop through all the prompts
+  const apiUrl = generateRandomApiUrl();
+  const form = generateFormData(promptArray[0], images.random);
   console.log('form: ', form)
   try {
-    const response = await fetch(API_URL, {
+    const response = await fetch(apiUrl, {
       method: 'POST',
       headers: headers,
       body: form
